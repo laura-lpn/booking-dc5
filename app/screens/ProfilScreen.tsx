824c92ea7e52a17e6ba1c8d50be96ed8fc0fee80
laura-lpn <74555001+laura-lpn@users.noreply.github.com>
2025-05-14 08:24:59 +0200

    feat(app): Implement user profile update functionality and user service

--- app/screens/ProfilScreen.tsx
+++ app/screens/ProfilScreen.tsx
@@ -1,9 +1,55 @@
 import { Text, View } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+import { useAuth } from "../hooks/useAuth";
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../context/AuthContext";
+import UserService from "../services/user.service";
 
 const ProfilScreen = () => {
+  const { signout } = useAuth();
+  const { user } = useContext(AuthContext);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  if (!user) {
+    return null;
+  }
+
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+    }
+  }, [user]);
+
+  if (!user) {
+    return null;
+  }
+
+  const handleUpdate = async () => {
+    try {
+      const updatedUser = await UserService.updateUser(user.id, {
+        ...user,
+        name,
+        email,
+      });
+      console.log("User updated successfully:", updatedUser);
+    } catch (error) {
+      console.error("Error updating user:", error);
+    }
+  };
+
   return (
     <View>
       <Text>Profil</Text>
+
+      <TextInput label="Nom" value={name} onChangeText={setName} />
+      <TextInput label="Email" value={email} onChangeText={setEmail} />
+
+      <Button onPress={handleUpdate}>Modifier</Button>
+
+      <Button onPress={signout}>Déconnexion</Button>
     </View>
   );
 };
---
+++ app/services/user.service.ts
@@ -0,0 +1,20 @@
+import api from "./api.service";
+
+const ENDPOINT = "/users";
+
+const fetchCurentUser = async () => {
+  const response = await api.get(`${ENDPOINT}/me`);
+  return response.data;
+};
+
+const updateUser = async (userId: string, data: any) => {
+  const response = await api.put(`${ENDPOINT}/${userId}`, data);
+  return response.data;
+};
+
+const UserService = {
+  fetchCurentUser,
+  updateUser,
+};
+
+export default UserService;
